45d45c65ea3de7780dd13568bae91b21119f0ff8
unknown <manuelguerreroinostroza@gmail.com>
2022-10-03 22:10:21 -0300

    less .sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,4 @@
+/*
 insert into customers (customer_id, rut,names,surnames,email, birthday) values (1,'1765487-2','Pedro Javier','Fernadez Inostroza','pjavier@mail.com', NOW());
 insert into customers (customer_id, rut,names,surnames,email, birthday) values (2,'2065487-k','David Marcelo','Morales','dmorales@mail.com', NOW());
 
@@ -10,3 +11,4 @@ insert into adresses (adress_customer,customer_id) values ('dakjdsalkjsda',1);
 
 insert into adresses (adress_customer,customer_id) values ('dakjdsalkjsda',1);
 insert into adresses (adress_customer,customer_id) values ('dakjdsalkjsda',2); 
+*/
